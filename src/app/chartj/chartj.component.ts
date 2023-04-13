3d4af544ac1cb6dc7b8cbfb20038df1b1ae7aa47
nounoua <Nour.Boukettaya.99@gmail.com>
2023-04-13 23:38:05 +0200

    finie chart js

--- src/app/chartj/chartj.component.ts
+++ src/app/chartj/chartj.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Chart, registerables } from 'chart.js';
 import { ChartConfiguration } from 'chart.js';
+
 
 
 @Component({
@@ -9,37 +9,38 @@ import { ChartConfiguration } from 'chart.js';
   styleUrls: ['./chartj.component.css']
 })
 export class ChartjComponent {
-    
-    title = 'ng2-charts-demo';
+  title = 'ng2-charts-demo';
 
-    public barChartLegend = true;
-    public barChartPlugins = [];
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40,80, 81, 56, 55, 40 ], label: 'Attendance' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90,80, 81, 56, 55, 40 ], label: 'Vacation' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90,80, 81, 56, 55, 40 ], label: 'Absence' }
+
+    ]
+  };
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
+  };
+
+  constructor(){}  
   
-    public barChartData: ChartConfiguration<'bar'>['data'] = {
-      labels:['January', 'February', 'March', 'April', 'May', 'June', 'July','August','Septembre','Octobre'
-      ,'November','December'
-      
-
-      ],
-      datasets: [
-        { data: [ 65, 59, 80, 81, 56, 55, 40,70,86, 27, 90,90 ], label: 'Vacation' },
-        { data: [ 28, 48, 40, 19, 86, 27, 90,90,86, 27, 90,90 ], label: 'Attendance' },
-        { data: [ 28, 48, 40, 19, 86, 27, 90,86, 27, 90,90,50 ], label: 'Leave' }
-
-      ],
-      
-    };
-  
-    public barChartOptions: ChartConfiguration<'bar'>['options'] = {
-      responsive: false,
-      scales: {
-        y: { // defining min and max so hiding the dataset does not change scale range
-          min: 0,
-          max: 100
-        }
-      }
-    };
-
-    constructor() {
-    }
 }
